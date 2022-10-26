82d8b52a36341caeb8ce19d3b6caf605a8c3edf1
Wish Suharitdamrong <peterwisu@gmail.com>
2022-10-26 17:27:59 +0100

    Draft work for ci

---
+++ model.py
@@ -0,0 +1,146 @@
+"""
+
+Computational Intelligence Coursework  
+
+Wish, Taimoor, Ionut 
+
+
+"""
+
+import torch 
+import torch.nn as nn 
+from torch.nn import MaxPool2d
+
+
+"""
+Main Classifier
+"""
+class Classifier(nn.Module):
+    def __init__(self):
+        
+        super(Classifier,self).__init__()
+
+        self.conv = nn.Sequential(Conv_block(in_channels=3, out_channels=64, kernel_size=(3,3), do_batchnorm=True),  # B , 64 , 28 , 28
+
+                                  MaxPool2d(3, (2,2)), # B, 64 , 14 ,14 
+
+                                  Conv_block(in_channels=64, out_channels=128, kernel_size=(3,3), do_batchnorm=True), #  B ,128 , 12 , 12
+
+                                  MaxPool2d(3, (2,2)), # B , 128 , 5 ,5 
+
+                                  Conv_block(in_channels=128, out_channels=256 ,kernel_size=(3,3), do_batchnorm=True), # B , 256 , 3 , 3
+
+                                  MaxPool2d(3, (2,2)),  # B , 256 , 1 , 1
+                                    
+                                  nn.Flatten()
+                        
+                )
+
+
+        self.fc = nn.Sequential(Linear_layer(256,1028, do_dropout=True, do_batchnorm=True, dropout_rate=0.25),
+                                Linear_layer(1028,256, do_dropout=True, do_batchnorm=True, dropout_rate=0.25),
+                                Linear_layer(256,10, do_activation=False) # No activation function in last layer since the softmax in already implement in CrossEntropy
+                                )
+
+
+    def forward(self, inputs): 
+
+        cnn_out = self.conv(inputs)
+        fc_out = self.fc(cnn_out)
+
+        return fc_out
+
+"""
+
+Linear Layer for Fully Connected Layers 
+
+"""
+class Linear_layer(nn.Module):
+
+    def __init__(self, in_features, out_features, do_dropout=False, do_batchnorm=False, do_activation=True, dropout_rate=None):
+
+        super().__init__()
+        self.linear =  nn.Linear(in_features, out_features)
+
+        self.do_activation = do_activation        
+
+        self.do_dropout = do_dropout
+
+        self.do_batchnorm = do_batchnorm
+
+        if self.do_activation:
+
+            self.activation = nn.ReLU()
+
+        if self.do_dropout:
+
+            self.drop_layer = nn.Dropout(dropout_rate)
+
+        if self.do_batchnorm:
+
+            self.batchnorm_layer = nn.BatchNorm1d(out_features)
+
+        
+    def forward(self, inputs):
+
+        linear_out = self.linear(inputs)
+
+        if self.do_activation:
+
+            linear_out = self.activation(linear_out)
+
+        if self.do_batchnorm:
+
+            linear_out = self.batchnorm_layer(linear_out)
+
+        if self.do_dropout:
+
+            linear_out = self.drop_layer(linear_out)
+
+
+        return  linear_out
+
+
+"""
+
+ Convolutional Layer (With batchnorm and dropout)
+
+"""
+class Conv_block(nn.Module):
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, do_dropout=False, do_batchnorm=False, dropout_rate=None):
+        
+        super().__init__()
+
+        self.conv_layer = nn.Conv2d(in_channels=in_channels, out_channels= out_channels, kernel_size= kernel_size, stride=stride , padding=padding)
+
+        self.activation = nn.ReLU()
+        
+        self.do_dropout = do_dropout
+
+        self.do_batchnorm = do_batchnorm
+
+        if self.do_dropout:
+
+            self.drop_layer = nn.Dropout2d(dropout_rate)
+
+        if self.do_batchnorm:
+
+            self.batchnorm_layer = nn.BatchNorm2d(out_channels)
+            
+
+    def forward(self, inputs):
+
+        cnn_out = self.conv_layer(inputs)
+
+        cnn_out = self.activation(cnn_out)
+
+        if self.do_batchnorm:
+
+            cnn_out = self.batchnorm_layer(cnn_out)
+
+        if self.do_dropout:
+
+            cnn_out = self.dropout_layer(cnn_out)
+        
+        return cnn_out
---
+++ train.py
@@ -0,0 +1,194 @@
+"""
+
+Computational Intelligence Coursework
+
+Name : Wish, Taimoor, Ionut
+
+
+"""
+
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader  as DataLoader 
+import matplotlib.pyplot as plt
+import cv2
+from tqdm import tqdm
+import torch.optim as optim
+from model import Classifier
+from torch.utils.tensorboard import SummaryWriter
+import matplotlib.pyplot as plt 
+import numpy as np
+
+
+savename = "CIFAR-10-SGD"
+writer = SummaryWriter("../CI_logs/{}".format(savename))
+
+
+"""
+
+    Train Model
+
+"""
+def train(model, device, loss_criterion, optimizer, training_set, testing_set,nepochs):
+    
+    global global_epochs
+
+    global_epochs = 1
+    
+    train_logs = np.array([])
+    eval_logs  = np.array([])
+
+    while global_epochs < nepochs+1:
+
+        progress_bar = tqdm(enumerate(training_set))
+        running_loss = 0
+        iter_inbatch = 0
+        for _ , (images, labels) in progress_bar:
+            
+            # move dataset to same device as model
+            images = images.to(device)
+            labels = labels.to(device)
+
+            # set model to training stage
+            model.train()
+
+            # reset optimizer
+            optimizer.zero_grad() 
+
+            # forward pass
+            predicted = model(images)
+
+            # calculate loss
+            loss = loss_criterion(predicted,labels) 
+
+            # backprop and optmizer update
+            loss.backward()
+            optimizer.step()
+             
+            running_loss +=loss.item()
+            iter_inbatch +=1
+            
+            
+            progress_bar.set_description("Training Epochs : {} , Loss : {}".format(global_epochs,(running_loss/iter_inbatch)))
+
+        # get loss in current iteration
+        train_loss = running_loss/iter_inbatch
+        eval_loss = eval(model, device, loss_criterion, testing_set)
+
+        # append in array 
+        train_logs = np.append(train_logs, train_loss)
+        eval_logs = np.append(eval_logs, eval_loss)
+
+        figure = plot_diff(train_logs, eval_logs)
+
+
+        writer.add_scalar("Loss/Train",train_loss,global_epochs)
+        writer.add_scalar("Loss/Eval",eval_loss,global_epochs)
+        writer.add_figure("Loss/plot",figure,global_epochs)        
+    
+        global_epochs +=1
+
+"""
+
+    Evaluate Model
+
+"""
+
+def eval(model, device, loss_criterion, testing_set):
+    
+    eval_progress_bar = tqdm(enumerate(testing_set))
+    eval_running_loss = 0 
+    eval_iter_inbatch = 0
+    
+    for _ , (images, labels) in eval_progress_bar:
+
+        images = images.to(device)
+        labels = labels.to(device)
+        #  Set model to evaluation stage
+        model.eval()
+
+        predicted = model(images)
+
+        # calculate loss
+        eval_loss = loss_criterion(predicted,labels)
+
+        eval_running_loss += eval_loss.item()
+
+        eval_iter_inbatch +=1
+
+        eval_progress_bar.set_description("Evaluation Epochs : {} , Loss : {}".format(global_epochs, (eval_running_loss/eval_iter_inbatch)))
+
+    
+    return eval_running_loss/eval_iter_inbatch
+
+"""
+Plot loss difference
+"""
+def plot_diff(train,evaluate):
+    
+    fig, (ax) = plt.subplots(nrows=1,ncols=1)
+    ax.plot(train, color='r' , label='Train')
+    ax.plot(evaluate, color='b', label='Eval')
+    ax.legend(loc="upper right")
+    ax.set_title("Loss")
+    figure = ax.get_figure()
+    plt.close(fig)
+    
+    return figure
+ 
+
+if __name__ == "__main__":
+    
+    #  Setup tensorboard
+    writer = SummaryWriter("../CI_logs/{}".format(savename))
+
+    device = "cuda" if torch.cuda.is_available else "cpu"
+
+    batch_size = 32
+
+    nepochs = 10
+
+    print("Using  **{}** as a device ".format(device))
+    print("Batch Size : {}".format(batch_size))
+    print("Iteration : {} epochs".format(nepochs))
+    
+
+    print("Loading dataset ....")
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))])
+    
+    # Prepare Dataset
+    training_set = torchvision.datasets.CIFAR10(root='./../data', train=True, download=True, transform=transform)
+
+    testing_set  = torchvision.datasets.CIFAR10(root='./../data', train=False, download=True, transform=transform)
+
+    train_loader = DataLoader(training_set, batch_size=batch_size, shuffle=True, num_workers=2)
+
+    test_loader = DataLoader(testing_set, batch_size=batch_size, shuffle=True, num_workers=2)
+
+    print("Training Dataset: {}".format(len(training_set)))
+    print("Testing Dataset: {}".format(len(testing_set)))
+    
+    # labels of dataset
+    classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    print("Classes in dataset  : {} ".format(classes))
+
+    
+    # Classifier Models
+
+    model = Classifier().to(device)
+
+    # Loss function Objective function 
+    CrossEntropy = nn.CrossEntropyLoss()
+
+    # Optimizer 
+    optimizer = optim.SGD(model.parameters(), lr=0.001)
+
+
+    train(model, device, CrossEntropy, optimizer, train_loader, test_loader, nepochs) 
+
+    writer.close()
+
